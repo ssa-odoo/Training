c4a2ba09b28606fc49ac4c5b11b93b1857ae0b00
sanaa salameh <ssa@odoo.com>
2021-01-31 11:22:01 +0400

    [ADD] ODOO

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Odoo Academy',
+    'version': '0.2',
+    'category': 'Training',
+    'description': 'Academy Module to manage Training: Courses, Sessions, Attendees',
+    'author': 'odoo',
+
+    'depends': ['base'],
+    'data': [
+        # data
+      
+
+        # views
+        
+    ],
+    'demo': [
+        
+    ],
+    
+}
