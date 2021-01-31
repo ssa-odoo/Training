65000104da0cf0cfe0789da090dd15952680deb3
sanaa salameh <ssa@odoo.com>
2021-01-31 12:01:42 +0400

    [ADD]add manifest and init file

---
+++ odoo_academy/models/course.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class Course(models.Model):
+    _name = 'academy.course'
+    _description = 'Course Info'
+
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Description')
+    level = fields.Selection([('beginner', 'Beginner'), ('intermediate', 'Intermediate'), ('advance', 'Advance')])
+    active = fields.Boolean(string='Active', default=True)
